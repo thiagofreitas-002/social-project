069671797b2234c28c080148b194f3ed3e1b688e
thiagofreitas-002 <thiagofreitas2909@gmail.com>
2022-12-06 10:20:43 -0300

    Responsividade adicionada

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,12 +3,12 @@ import * as C from './styles'
 export function Home() {
   return (
     <C.Container>
-      <div className="sub-container">
-        <span>SITE OFICIAL AFS</span>
-        <h1 className='bem'>BEM</h1>
-        <h1 className='vindo'>VINDO</h1>
+      <div className="content">
+        <span className="title">SITE OFICIAL AFS</span>
+        <h1>
+          BEM<span className="vindo">VINDO</span>
+        </h1>
       </div>
-      
     </C.Container>
   )
 }
--- src/pages/home/styles.js
+++ src/pages/home/styles.js
@@ -1,5 +1,5 @@
-import styled from "@emotion/styled";
-import background from "../../assets/bg.png";
+import styled from '@emotion/styled'
+import background from '../../assets/bg.png'
 
 export const Container = styled.div`
   background-image: url(${background});
@@ -7,37 +7,52 @@ export const Container = styled.div`
   background-size: cover;
   background-repeat: no-repeat;
 
-  .sub-container {
+  .content {
     position: absolute;
     margin-left: 15%;
     margin-top: 25%;
 
     h1 {
-      font-size: 200px;
+      text-align: left;
+      font-size: 12.5rem;
       font-weight: bold;
       line-height: 80%;
       letter-spacing: -0.02em;
       mix-blend-mode: multiply;
-    }
-
-    .bem {
       color: rgba(255, 153, 0, 0.8);
+
+      .vindo {
+        color: rgba(0, 94, 26, 0.8);
+        display: block;
+      }
     }
 
-    .vindo {
-      color: rgba(0, 94, 26, 0.8);
-    }
-
-    span {
+    .title {
       color: rgba(255, 153, 0, 0.8);
-      font-family: "Montserrat";
+      font-family: 'Montserrat';
       font-style: normal;
       font-weight: 600;
-      font-size: 64px;
+      font-size: 4rem;
       line-height: 80.4%;
       letter-spacing: -0.065em;
 
       mix-blend-mode: multiply;
     }
   }
-`;
+
+  @media (max-width: 425px) {
+    .content {
+      padding: 25px 30px;
+      margin-top: 0;
+      margin-left: 0;
+
+      .title {
+        font-size: 2rem;
+      }
+
+      h1 {
+        font-size: 5.5rem;
+      }
+    }
+  }
+`
