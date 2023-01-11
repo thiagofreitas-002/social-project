ffeeea44e934f06dc9ce1268eb71e831000a6c6d
Artur Bruno <arturjuliao20@gmail.com>
2023-01-11 20:06:21 -0300

    fix image position

--- src/pages/NoticesDetails/styles.js
+++ src/pages/NoticesDetails/styles.js
@@ -1,14 +1,16 @@
 import styled from '@emotion/styled'
 export const Container = styled.div`
   font-family: var(--notices-datails-font);
-
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
   img {
-    width: 100%;
-    height: 50vh;
+    width: calc(2/3 * 100vw);
     object-fit: cover;
     filter: blur(0.03rem);
     transition: 200ms;
-
+    aspect-ratio: 16 / 9;
     &:hover {
       filter: blur(0);
     }
@@ -19,12 +21,10 @@ export const Content = styled.div`
   max-width: 1368px;
   padding: 0 2rem;
   min-height: 100vh;
-  margin: 0 auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: center;
-
   #title {
     color: #111111;
     font-weight: 700;
@@ -33,7 +33,6 @@ export const Content = styled.div`
     padding: 0 2rem;
     text-align: center;
   }
-
   @media (max-width: 425px) {
     padding: 0 0.5rem;
   }
@@ -51,25 +50,21 @@ export const TextContent = styled.div`
   justify-content: space-between;
   align-items: flex-end;
   min-height: 50vh;
-
   > p {
     font-size: clamp(1.2rem, 3vw, 1.25rem);
     line-height: 1.5rem;
     font-weight: 400;
     color: #333333;
   }
-
   > a {
     color: #245633;
     text-decoration: underline;
   }
-
   > a[href='/notices'] {
     display: inline-flex;
     align-items: center;
     justify-content: center;
     text-decoration: none;
-
     color: #000;
     font-weight: 500;
     padding: 0.375rem;
@@ -78,7 +73,6 @@ export const TextContent = styled.div`
     background: #ff9900;
     text-transform: uppercase;
     transition: 200ms;
-
     &:hover {
       filter: brightness(0.9);
       box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
